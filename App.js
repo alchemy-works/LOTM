e7469c436664beeb2b9528f1918f06b3f3f1eb3b
cbdyzj <cbdyzj@jianzhao.org>
2021-05-23 22:31:32 +0800

    Init

---
+++ App.js
@@ -0,0 +1,14 @@
+import { html, styled, marked, useRef, useState } from './modules.js'
+
+const Container = styled.div`
+`
+
+export default function App(props) {
+
+
+    return html`
+        <${Container}>
+          LOTM
+        </Container>
+    `
+}
